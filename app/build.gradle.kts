ef5a3d8c8df3bf39f243cdd3472aeeb927e5447b
tgvdufuture <contact.clowntrone.minipato@gmail.com>
2026-08-17 19:11:48 +0200

    Bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aura.led"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildFeatures {
