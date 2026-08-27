ebc20c520a430517a0375767c9af329e978fd1a4
tgvdufuture <contact.clowntrone.minipato@gmail.com>
2026-08-27 14:32:18 +0200

    Prepare v0.4.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aura.led"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
     }
 
     buildFeatures {
